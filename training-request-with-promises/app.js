21aa774c07183fd549b8e5c750f00bb53b8068c5
Renan Maia <renanmaia.rl@gmail.com>
2022-12-23 14:07:13 -0300

    training javascript. GOD IS GOOD!

--- training-request-with-promises/app.js
+++ training-request-with-promises/app.js
@@ -4,12 +4,19 @@ const request = new XMLHttpRequest();
 
 request.addEventListener('readystatechange', () => {
 
-    console.log(request.responseText);
+    if (request.readyState === 4 & request.status === 200) {
+        console.log(request.responseText);
+    }
+
+
+    if (request.readyState === 4) {
+        console.log('Não foi possivel obter os dados da API')
+    }
 
 
 });
 
 
 
-request.open('GET', 'https://pokeapi.co/api/v2/pokemon/ditto');
+request.open('GET', 'https://pokeapi.co/api/v2/pokemon/1');
 request.send();
