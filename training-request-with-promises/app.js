a5aa34d113e60df27a79b9d7aaeb0773b313b248
Renan Maia <renanmaia.rl@gmail.com>
2022-12-25 20:52:21 -0300

    training javascript. GOD IS GOOD!

--- training-request-with-promises/app.js
+++ training-request-with-promises/app.js
@@ -1,51 +1,42 @@
 
-const getPokemons = url => new Promise((resolve, reject) => {
-    
+    const getPokemons = url => new Promise((resolve, reject) => {
+
     const request = new XMLHttpRequest();
-    
+
     request.addEventListener('readystatechange', () => {
-    
+
         if (request.readyState === 4 && request.status === 200) {
             const data = JSON.parse(request.responseText);
             resolve(data.name);
-            
         }
-    
-        if (request.readyState === 4) {
-            reject('Não foi possivel obter os dados da API');
 
+        if (request.readyState === 4) {
+            reject('Não foi possível obter os dados da API');
         }
-    
     });
-    
-    
+
     request.open('GET', url);
     request.send();
 
 });
 
 
-getPokemons('https://pokeapi.co/api/v2/pokemon/1')
-    .then(pokemon => {
-        console.log(pokemon);
+getPokemons('https://pokeapi.co/api/v2/pokemon/25')
+    .then(pikachu => {
+        console.log(pikachu);
+        return getPokemons('https://pokeapi.co/api/v2/pokemon/1');
+    })
+    .then(bulbasaur => {
+        console.log(bulbasaur);
+        return getPokemons('https://pokeapi.co/api/v2/pokemon/4');
+    })
+    .then(charmander => {
+        console.log(charmander);
     })
     .catch(error => {
         console.log(error);
-    })
+    });
 
-
-
-
-
-
-// getPokemons('https://pokeapi.co/api/v2/pokemon/1', (error, data) => {
-//     if (error) {
-//         console.log(error);
-//         return;
-//     }
-
-//     console.log(data);
-// });
 
 
 
