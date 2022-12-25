98b4a470e61eb5fbf355f69ae5b6896aeb45b409
Renan Maia <renanmaia.rl@gmail.com>
2022-12-24 23:16:04 -0300

    training javascript. GOD IS GOOD!

--- training-request-with-promises/app.js
+++ training-request-with-promises/app.js
@@ -1,18 +1,18 @@
 
-const getPokemons = (url, callback) => {
-
+const getPokemons = url => new Promise((resolve, reject) => {
+    
     const request = new XMLHttpRequest();
     
     request.addEventListener('readystatechange', () => {
     
         if (request.readyState === 4 && request.status === 200) {
             const data = JSON.parse(request.responseText);
-            callback(null, data);
+            resolve(data);
             return
         }
     
         if (request.readyState === 4) {
-            callback('Não foi possivel obter os dados da API', null);
+            reject('Não foi possivel obter os dados da API');
         }
     
     });
@@ -21,16 +21,30 @@ const getPokemons = (url, callback) => {
     request.open('GET', url);
     request.send();
 
-}
-
-getPokemons('https://pokeapi.co/api/v2/pokemon/1', (error, data) => {
-    if (error) {
-        console.log(error);
-        return;
-    }
-
-    console.log(data);
 });
+
+
+getPokemons('https://pokeapi.co/api/v2/pokemon/1')
+    .then(pokemon => {
+        console.log(pokemon);
+    })
+    .catch(error => {
+        console.log(error);
+    })
+
+
+
+
+
+
+// getPokemons('https://pokeapi.co/api/v2/pokemon/1', (error, data) => {
+//     if (error) {
+//         console.log(error);
+//         return;
+//     }
+
+//     console.log(data);
+// });
 
 
 
